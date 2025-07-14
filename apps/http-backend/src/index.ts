109b8c665b1aed28ad48776d927aa49c150529de
Neel Khatri <neelkhatri7780@gmail.com>
2025-07-15 00:38:01 +0530

    Updated

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hkknef")
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        "message" : "Welcome!"
+    })
+})
+
+app.listen(4000)
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port : 8080 })
+
+wss.on("connection", (socket) => {
+    console.log("User connected!")
+    socket.on("message", (e) => {
+        socket.send("pong")
+    })
+})
